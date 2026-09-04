f1011570b0022792beaad210cc137844b9d23bf9
Reaper754 <MasonT019@stemcollegiate.ca>
2026-09-04 10:07:47 -0600

    Update score_analyzer.py

--- score_analyzer.py
+++ score_analyzer.py
@@ -1,3 +1,5 @@
+#from linear_search import linear_search
+
 scores = [72, 91, 64, 85, 78, 95, 68, 88]
 def calculate_average(scores):
     first = 0
@@ -10,11 +12,29 @@ def calculate_average(scores):
     first=first/8
     return first
 
-def linear_search(values, target):
-    for i in range(len(scores)):
-        if scores[i] == target:
+def linear_score_search(values, target):
+    for i in range(len(values)):
+        if values[i] == target:
             return i
 
     return -1
 
-print(calculate_average(scores))
+def bubble_sort(values):
+    for number in range(len(values)-1):
+        for i in range(len(values)-1-number):
+            if values[i] > values[i+1]:
+                temp=values[i]
+                values[i]=values[i+1]
+                values[i+1]=temp
+
+
+print("Scores:", scores)
+
+print("Average:", calculate_average(scores))
+
+write = int(input("Enter a score to search for: "))
+search = linear_score_search(scores, write)
+print(scores[search], f"was found at index {search}")
+
+bubble_sort(scores)
+print("Sorted scores:", scores)
